e340598758d3cd4262b8686188ea7100065d31bf
Jai Singh <jai.ss0023@gmail.com>
2022-05-31 20:01:08 +0530

    first commit

---
+++ DEMOWebApi/DEMOWebApi/MapperProfile/AppMapProfile.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Demo.Dtos.Common;
+using Demo.Dtos.Request;
+using Demo.Dtos.Response;
+using Demo.Models;
+using Demo.ViewModels.Common;
+using Demo.ViewModels.Request;
+using Demo.ViewModels.Response;
+
+namespace DEMOWebApi.MapperProfile
+{
+    public class AppMapProfile : Profile
+    {
+        public AppMapProfile()
+        {
+            
+            CreateMap<AddStudentViewModel, AddStudentDto>();
+            CreateMap<AddStudentDto, Student>();
+
+            CreateMap<EditStudentViewModel, EditStudentDto>().ReverseMap();
+            CreateMap<Student, EditStudentDto>();
+
+            CreateMap<StudentMarks, ReturnStudentMarksDto>();
+            CreateMap<ReturnStudentMarksDto, ReturnStudentMarksViewModel>();
+
+            CreateMap<AddStudentMarksViewModel, AddStudentMarksDto>();
+            CreateMap<AddStudentMarksDto, StudentMarks>();
+
+            CreateMap<EditStudentMarksViewModel, EditStudentMarksDto>();
+            CreateMap<StudentMarks, EditStudentMarksDto>();
+
+
+
+
+
+            CreateMap<GetFilteredRecordsViewModel, GetFilteredRecordsDto>();
+            CreateMap<SearchFilterViewModel, SearchFilterDto>();
+
+            CreateMap<StudentMarks, ReturnStudentMarksDto>();
+            CreateMap<ReturnStudentMarksDto, ReturnStudentMarksViewModel>();
+
+            
+            CreateMap<Student, EditStudentDto>();
+
+
+        }
+    }
+}
---
+++ DEMOWebApi/Demo.Services/Interface/IStudentService.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Common;
+using Demo.Dtos.Request;
+using Demo.Dtos.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Services.Interface
+{
+    public interface IStudentService
+    {
+         
+        Task<Guid> AddStudent(AddStudentDto registerStudentDto);
+        Task<Guid> EditStudent(EditStudentDto editStudentDto);
+        Task<Guid> DeleteStudent(DeleteEntityDto deleteEntityDto);
+
+        Task<MultipleRecordsAndCount<IEnumerable<ReturnStudentMarksDto>>> GetMultipleStudentRecords(GetFilteredRecordsDto getStudenttRecordsDto);
+
+    }
+}
